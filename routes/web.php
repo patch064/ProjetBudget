f9be2a7935c296d4f71a2fc69c178e4533494f04
NovaCookie <95030874+NovaCookie@users.noreply.github.com>
2022-03-15 17:28:13 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-});
 
 Route::resource('budget',BudgetController::class);
 Route::resource('projet',ProjetController::class);
